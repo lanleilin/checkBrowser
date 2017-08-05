fe7103f55ab271d5b0b182d613166e4b9dd1b937
lanleilin <2476081528@qq.com>
2017-08-05 11:51:12 +0800

    refraction

--- js/check.js
+++ js/check.js
@@ -14,19 +14,23 @@ window.onload=function(){
 		document.body.style.color='#FFFFFF';
 	}
 	function setColor(){
-		var array=[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
-		var lanH1=document.getElementsByClassName('platform')[0];
-		lanH1.style.color='#'+
-		array[parseInt(Math.random()*16)]+
-		array[parseInt(Math.random()*16)]+
-		array[parseInt(Math.random()*16)]+
-		array[parseInt(Math.random()*16)]+
-		array[parseInt(Math.random()*16)]+
-		array[parseInt(Math.random()*16)];
+		this.arr=[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
+		this.lanH1=document.getElementsByClassName('platform')[0];
+		this.title=document.getElementById('title');
+		this.color='#'+
+		this.arr[parseInt(Math.random()*16)]+
+		this.arr[parseInt(Math.random()*16)]+
+		this.arr[parseInt(Math.random()*16)]+
+		this.arr[parseInt(Math.random()*16)]+
+		this.arr[parseInt(Math.random()*16)]+
+		this.arr[parseInt(Math.random()*16)];
+		this.lanH1.style.color=this.color;
+		this.title.style.color=this.color;
+		
 	}
-	setColor();
+	new setColor();
 	clearInterval();
 	setInterval(function(){
-		setColor();
+		new setColor();
 	},10000);
 }
